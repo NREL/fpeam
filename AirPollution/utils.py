eacfd2c3119b36b8559e526babb376fc191a18de
Dylan Hettinger <dylan.hettinger@nrel.gov>
2016-01-07 10:48:50 -0700

    complete config file integration; lay groundwork for native logging

--- AirPollution/utils.py
+++ AirPollution/utils.py
@@ -1,4 +1,4 @@
-import configobj
+import configobj, validate
 import os
 import logging
 import sys
@@ -8,31 +8,52 @@ import getpass
 # get current date
 cdate = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 
-# get configuration info
+# get scenario configuration info from first command-line argument if provided, otherwise use default
 try:
     cfile = os.path.abspath(sys.argv[1])
 except IndexError:
     cfile = os.path.abspath('config.ini')
 
+# get database configuration info from second command line argument if provided, otherwuse use default
 try:
     dfile = os.path.abspath(sys.argv[2])
 except IndexError:
     dfile = os.path.abspath('database.ini')
 
-try:
-    config = configobj.ConfigObj(cfile, file_error=True)
-except (configobj.ConfigObjError, IOError), e:
-    sys.exit('Error with config file {cfile}: {e}'.format(cfile=cfile, e=e))
+# set config specification file for validating
+cspec_file = os.path.abspath('configspec.ini')
 
+# load scenario config
+try:
+    config = configobj.ConfigObj(cfile, configspec=cspec_file, file_error=True)
+except (configobj.ConfigObjError, IOError), e:
+    sys.exit(e)
+
+# load database config
 try:
     d_config = configobj.ConfigObj(dfile, file_error=True)
 except (configobj.ConfigObjError, IOError), e:
-    sys.exit('Error with database config file {cfile}: {e}'.format(cfile=cfile, e=e))
+    sys.exit(e)
 
+# merge configs
 try:
     config.merge(d_config)
 except configobj.ConfigObjError, e:
     sys.exit('Error combining config files {cfile} and {dfile}: {e}'.format(cfile=cfile, e=e, dfile=dfile))
+
+# init config validator
+validator = validate.Validator()
+# validate config against cspec_file
+valid_config = config.validate(validator)
+
+# if errors in config, log and exit
+if valid_config is not True:
+    for (section_list, key, _) in configobj.flatten_errors(config, valid_config):
+        if key is not None:
+            print 'The "%s" key in the section "%s" failed validation' % (key, ', '.join(section_list))
+        else:
+            print 'The following section was missing: %s ' % ', '.join(section_list)
+    sys.exit('Check configuration file(s) for errors')
 
 
 def initialize_logger(output_dir=os.getcwd(), level=None):
@@ -54,8 +75,8 @@ def initialize_logger(output_dir=os.getcwd(), level=None):
 
     # set level if not already specified as a parameter or in the config
     if level is None:
-        if config.has_key('logging_level'):
-            level = config.get('logging_level')
+        if config.has_key('logger_level'):
+            level = config.get('logger_level')
         else:
             level = 'INFO'
 
@@ -71,7 +92,12 @@ def initialize_logger(output_dir=os.getcwd(), level=None):
     logger.addHandler(handler)
 
     # create error file handler
-    log_file = '{u}_{f}_{d}.log'.format(u=getpass.getuser(), f=config.filename[:-4], d=cdate)
+    config_root = os.path.dirname(config.filename)
+    _ = {'u': getpass.getuser(),
+         'f': os.path.splitext(os.path.basename(config.filename))[0],
+         'd': cdate}
+
+    log_file = os.path.join(config_root, '{f}_{u}_{d}.log'.format(**_))
     handler = logging.FileHandler(os.path.join(output_dir, log_file), 'w', encoding=None, delay='true')
     handler.setLevel(log_levels[level])
 
