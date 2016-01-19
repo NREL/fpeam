78007197a45eaa2526f05ece8a2a8b2f303b7093
Annika Eberle <Annika.Eberle@nrel.gov>
2016-01-19 16:19:17 -0700

    Fixed error in query
    Removed " before fug_pm10 and fug_pm25

--- AirPollution/FugitiveDust.py
+++ AirPollution/FugitiveDust.py
@@ -398,9 +398,8 @@ class SG_FugitiveDust(SaveDataHelper.SaveDataHelper):
                 query = """
                         UPDATE "sg_raw" raw
                         SET 
-                            "fug_pm10 = """ + str("(" + k10 + "* "+ D +" * 0.907/2000 * ((tfd.silt / 12)^" + a10 + ") * ((" + weight + "/3)^" + b10 + "))/"+ rot+"") + """,
-                            "fug_pm25 = """ + str("(" + k25 + "* "+ D +" * 0.907/2000 * ((tfd.silt / 12)^" + a25 + ") * ((" + weight + "/3)^" + b25 + "))/"+ rot+"") + """
-
+                            fug_pm10 = """ + str("(" + k10 + "* "+ D +" * 0.907/2000 * ((tfd.silt / 12)^" + a10 + ") * ((" + weight + "/3)^" + b10 + "))/"+ rot+"") + """,
+                            fug_pm25 = """ + str("(" + k25 + "* "+ D +" * 0.907/2000 * ((tfd.silt / 12)^" + a25 + ") * ((" + weight + "/3)^" + b25 + "))/"+ rot+"") + """
                         FROM """ + self.db.constants_schema + """."transportfugitivedust" tfd
                         WHERE     (raw."fips" ILIKE tfd."fips" || '%') AND
                                   (raw."run_code" ILIKE '""" + str("%" + self.description + str(year + 1) + "%") + """')"""
