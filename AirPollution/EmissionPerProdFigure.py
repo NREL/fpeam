64a370d052d61f50134bd50f65ea987a2c05b2ab
Dylan Hettinger <dylan.hettinger@nrel.gov>
2016-01-26 14:06:35 -0700

    cleanup

--- AirPollution/EmissionPerProdFigure.py
+++ AirPollution/EmissionPerProdFigure.py
@@ -1,16 +1,27 @@
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from pylab import *
+from pylab import *  # @TODO: remove global scope import
 import matplotlib.pyplot as plt
 from scipy.stats import scoreatpercentile
 import os
 
+
+# @TODO: refactor to match PEP8 standards
+# @TODO: refactor to use string formatting
+# @TODO: fill out docstrings
+
 class EmissionPerProdFigure(object):
+    """
+
+    """
+
     def __init__(self, cont):
         """
         Create emmision graphs per a acre..
-        @param db: Database.
-        @param path: Directory path.
+
+        :param cont:
+        :return:
         """
+
         self.path = cont.get('path')
         self.db = cont.get('db')
         self.document_file = "EmissionsPerProdFigure"
@@ -18,9 +29,9 @@ class EmissionPerProdFigure(object):
         # define inputs/constants:
         pollutant_labels = ['$PM_{10}$', '$PM_{2.5}$']
         # the order of the 2 list has to be the same.
-        feedstock_list = ['Corn Grain', 'Switchgrass', 'Corn Stover', 'Wheat Straw', 'Forest Residue']
-        f_list = ['CG', 'SG', 'CS', 'WS', 'FR']
-        pollutant_list = ['pm10', 'pm25']
+        feedstock_list = ['Corn Grain', 'Switchgrass', 'Corn Stover', 'Wheat Straw', 'Forest Residue']  # @TODO: remove hardcoded values
+        f_list = ['CG', 'SG', 'CS', 'WS', 'FR']  # @TODO: remove hardcoded values
+        pollutant_list = ['pm10', 'pm25']  # @TODO: remove hardcoded values
         
         for p_num, pollutant in enumerate(pollutant_list):
             # -----------------EXTRACT DATA FROM THE DATABASE-----------------
@@ -44,7 +55,7 @@ class EmissionPerProdFigure(object):
             # plt.yscale('log')
     
             plot_title = pollutant_labels[p_num]
-            axis_title = '%s emissions (mt fugitive dust/ (dt for cellulosic, bushel for corn grain) feedstock)' % (pollutant_labels[p_num],)
+            axis_title = '%s Emissions (mt fugitive dust / (dt for cellulosic, bushel for corn grain) feedstock)' % (pollutant_labels[p_num], )
             
             self.__set_axis__(plot_title, axis_title, data_array, f_list)    
             # plot 95% intervals 
@@ -52,9 +63,16 @@ class EmissionPerProdFigure(object):
     
             fig.savefig(self.path + 'Figures' + os.sep + 'EmissionsPerProd_' + pollutant + '.png', format='png')
     
-            print pollutant
+            print pollutant  # @TODO: convert to logger
 
     def __collect_data__(self, feedstock_list, pollutant, f_list):
+        """
+
+        :param feedstock_list:
+        :param pollutant:
+        :param f_list:
+        :return:
+        """
         data = []
         for fNum, feedstock in enumerate(feedstock_list):
 
@@ -83,6 +101,11 @@ class EmissionPerProdFigure(object):
         return data
 
     def __plot_interval__(self, data_array):
+        """
+
+        :param data_array:
+        :return:
+        """
 
         num_feed = 4
         num_array = array([x for x in range(num_feed)]) + 1  # index starts at 1, not zero
@@ -105,11 +128,18 @@ class EmissionPerProdFigure(object):
     def __set_axis__(self, plot_title, axis_title, data_array, data_labels):
         """
         function to set axis titles and plot titles
+
+        :param plot_title:
+        :param axis_title:
+        :param data_array:
+        :param data_labels:
+        :return:
         """
         # Add a horizontal grid to the plot
         self.ax1.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.7)
                       
         # determine limits of axis
+        # @TODO: refactor so users don't have to uncomment code to change plot
         # to view all, let it be logarithim scale: self.ax1.set_ylim(bottom=1e-07, top=1e-01)
         # to view sg, cs, and ws spread: self.ax1.set_ylim(bottom=1e-07, top=0.0002
         # to view cg spread: self.ax1.set_ylim(bottom=0.0002, top=.003)     
@@ -124,14 +154,15 @@ class EmissionPerProdFigure(object):
         self.ax1.set_xticklabels(data_labels, size=25, style='normal')
 
 if __name__ == "__main__": 
-    from model.Database import Database
-    import Container
-
-    title = 'sgNew'
-    cont = Container.Container()
-    cont.set('path', 'C:/Nonroad/%s/' % (title,))
-    cont.set('db', Database(title))
-
-    # Emissions per a production lb figure.
-    print 'Creating emissions per lb figure.'
-    EmissionPerProdFigure(cont)
+    # from model.Database import Database
+    # import Container
+    #
+    # title = 'sgNew'
+    # cont = Container.Container()
+    # cont.set('path', 'C:/Nonroad/%s/' % (title,))
+    # cont.set('db', Database(title))
+    #
+    # # Emissions per a production lb figure.
+    # print 'Creating emissions per lb figure.'
+    # EmissionPerProdFigure(cont)
+    raise NotImplementedError
