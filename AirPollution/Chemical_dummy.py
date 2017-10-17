05d42885df1aeeb290769c05f955fcc5b9cd665f
rjhanes <rebecca.hanes@nrel.gov>
2017-10-17 16:22:12 -0600

    changes file paths from all hard-coded to partially hard-coded with getcwd

--- AirPollution/Chemical_dummy.py
+++ AirPollution/Chemical_dummy.py
@@ -3,6 +3,8 @@
 
 """
 import pandas as pd
+# remove os once csv files are no longer needed
+import os
 
 # Nitro fertilizer emissions factors, Units: percent volatilized
 nfert_ef = (1./100.) * pd.DataFrame({'nox': [0.79, 3.8, 3.5, 0.9, 0.79], 
@@ -29,12 +31,12 @@ nfert_ems = pd.DataFrame({"nox_ef": nfert_app.multiply(nfert_ef['nox'],
 nfert_ems['crop'] = nfert_ems.index
 
 # read in budget data for energy crops
-budg_en = pd.read_csv('C:/Users/rhanes/Documents/GitHub/fpeam/input_data/' + 
-                        'equip_budgets/bdgtengy_20160114.csv')
+budg_en = pd.read_csv(os.getcwd() + '\\input_data\\' +
+                        'equip_budgets\\bdgtengy_20160114.csv')
 
 # budget has CT, NT
-budg_conv = pd.read_csv('C:/Users/rhanes/Documents/GitHub/fpeam/input_data/' + 
-                        'equip_budgets/bdgtconv_20151215.csv')
+budg_conv = pd.read_csv(os.getcwd() + '\\input_data\\' +
+                        'equip_budgets\\bdgtconv_20151215.csv')
 
 # pull out CT budget entries to create proxy entries for RT
 budg_conv_append = budg_conv[budg_conv.till == 'CT']
@@ -50,8 +52,8 @@ budg = budg_conv.append(budg_en)
 del(budg_conv, budg_en)
 
 # read in complete production data set
-prod_dat = pd.read_csv('C:/Users/rhanes/Documents/GitHub/fpeam/input_data/' + 
-                    'current_ag/bc10602040prod_20160126.csv')
+prod_dat = pd.read_csv(os.getcwd() + '\\input_data\\' +
+                    'current_ag\\bc10602040prod_20160126.csv')
                     
 # rename the prod column b/c prod is a built-in method for data frames
 # prod calculates the product, btw
