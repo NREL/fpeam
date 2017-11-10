1b1258e00e4a72852acb7005df3d1bdebb752ff6
rjhanes <rebecca.hanes@nrel.gov>
2017-11-10 12:25:18 -0700

    completes initial version of logistics module prototype

--- AirPollution/Logistics_prototype.py
+++ AirPollution/Logistics_prototype.py
@@ -1,59 +1,194 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Feb 29 1:02:31 2016
+Prototyped on Wednesday November 8 2017
 
-Populates tables for logistics emissions and electricity consumption associated with feedstock processing
+Populates tables for logistics emissions and electricity consumption
+associated with feedstock processing
 
 Inputs include:
     list of feedstock types (feedstock_list)
     container (cont)
 
-@author: aeberle
+@author: aeberle, rhanes
 """
 
-import SaveDataHelper
-from utils import config, logger
+import pandas as pd
 
+# @TODO remove when finalizing
+import os
 
-class Logistics(SaveDataHelper.SaveDataHelper):
+class Logistics:
     """
-    Computes the emissions and electricity use associated with feedstock processing
-    Only emissions are VOC from wood drying (forestry only)
-    Electricity is tabulated to compute total kWh by feedstock type
+    - Computes the emissions and electricity use associated with feedstock
+    processing
+    - Only emissions are VOC from wood drying (forestry only)
+    - Electricity is tabulated to compute total kWh by feedstock type
     """
 
-    def __init__(self, feedstock_list, cont):
+    def __init__(self):
         """
+        Prototype edition:
+        Defines electricity usage (kWh) per dry ton
+        Defines VOC emission factors for wood drying
+        Reads in relevant columns from all transport data tables,
+        adds identification columns and binds together into one transport table
 
-        :param feedstock_list: list of feedstocks to process
-        :param cont: Container object
+        Final edition:
+        Pulls in electricity usage (kWh) per dry ton from default or
+        user-defined data set
+        Pulls in VOC emission factors for wood drying from default or
+        user-defined data set
+        Pulls in relevant columns from default or user-defined transport
+        data tables, adds ID columns and binds together
+
         :return:
         """
-        SaveDataHelper.SaveDataHelper.__init__(self, cont)
-        self.feedstock_list = feedstock_list
-        self.document_file = "Logistics"  # gets used to save query to a text file for debugging purposes
-        self.electricity_per_dt = config.get('electricity_per_dt')  # electricity dictionary
 
-        self.kvals = cont.get('kvals')
+        # hard-coded electricity usage (kWh) per dry ton biomass under
+        # conventional and advanced logistics
+        self.electricity_per_dt = pd.DataFrame({'feed_id': ['Corn stover',
+                                                             'Corn stover',
+                                                             'Wheat straw',
+                                                             'Wheat straw',
+                                                             'Switchgrass',
+                                                             'Switchgrass',
+                                                             'Miscanthus',
+                                                             'Miscanthus',
+                                                             'Corn grain',
+                                                             'Corn grain',
+                                                             'Sorghum stubble',
+                                                             'Sorghum stubble',
+                                                             'Residues',
+                                                             'Residues',
+                                                             'Whole tree',
+                                                             'Whole tree'],
+                                                 'logistics_type': ['conv', 'adv',
+                                                                    'conv', 'adv',
+                                                                    'conv', 'adv',
+                                                                    'conv', 'adv',
+                                                                    'conv', 'adv',
+                                                                    'conv', 'adv',
+                                                                    'conv', 'adv',
+                                                                    'conv', 'adv',],
+                                                 'kWh_per_dt': [36.0, 188.5,
+                                                                36.0, 188.5,
+                                                                36.0, 188.5,
+                                                                36.0, 188.5,
+                                                                36.0, 188.5,
+                                                                21.0, 173.5,
+                                                                40.0, 191.5,
+                                                                40.0, 191.5]})
 
-        # get dictionary for production column names
-        # self.column_dict = config.get('prod_column_dict')
+        # hard-coded VOC emission factors for wood drying
+        # @todo what are these units, kg/metric ton?
+        self.voc_wood_ef = pd.DataFrame({'feed_id': ['Residues', 'Residues',
+                                                     'Whole tree',
+                                                     'Whole tree'],
+                                         'logistics_type': ['conv', 'adv',
+                                                            'conv', 'adv'],
+                                         'hammer_mill_ef': [0.52, 0.52,
+                                                            0.52, 0.52],
+                                         'grain_dryer_ef': [0.0, 0.23,
+                                                            0.0, 0.23]})
 
-        # dictionary for VOC emission factor from wood drying
-        self.voc_wood_ef = config.get('voc_wood_ef')
+        ## read in the four relevant transport tables for 2040,
+        # add three extra ID columns for year, logistics type and yield
+        # type, then bind together into one comprehensive transport
+        # table for use elsewhere
+        transport_herb_bc_adv = pd.read_csv(os.getcwd()\
+                                            + '\\input_data\\' \
+                                            + 'logistics_transport_data' \
+                                            + '\\transport_herb_bc_adv_' \
+                                            + '2040.csv',
+                                            usecols = ['feed_id',
+                                                       'sply_fips',
+                                                       'used_qnty'])
+        transport_herb_bc_adv['year'] = 2040
+        transport_herb_bc_adv['logistics_type'] = 'adv'
+        transport_herb_bc_adv['yield_type'] = 'bc'
 
-        # set feedstock id table for transportation data
-        self.transport_feed_id_dict = config.get('feed_id_dict')
+        transport_herb_hh_adv = pd.read_csv(os.getcwd() \
+                                              + '\\input_data\\' \
+                                              + 'logistics_transport_data' \
+                                              + '\\transport_herb_hh_adv_' \
+                                              + '2040.csv',
+                                             usecols = ['feed_id',
+                                                        'sply_fips',
+                                                        'used_qnty'])
+        transport_herb_hh_adv['year'] = 2040
+        transport_herb_hh_adv['logistics_type'] = 'adv'
+        transport_herb_hh_adv['yield_type'] = 'hh'
 
-        # get dictionaries for transportation data tables
-        self.feed_type_dict = config.get('feed_type_dict')  # dictionary of feedstock types
-        self.transport_table_dict = config.get('transport_table_dict')
+        transport_woody_bc_adv = pd.read_csv(os.getcwd() \
+                                             + '\\input_data\\' \
+                                             + 'logistics_transport_data' \
+                                             +
+                                              '\\transport_woody_bc_adv_' \
+                                             + '2040.csv',
+                                              usecols = ['feed_id',
+                                                         'sply_fips',
+                                                         'used_qnty'])
+        transport_woody_bc_adv['year'] = 2040
+        transport_woody_bc_adv['logistics_type'] = 'adv'
+        transport_woody_bc_adv['yield_type'] = 'bc'
 
-        # get yield type for scenario
-        self.yield_type = config.get('yield')
+        transport_woody_hh_adv = pd.read_csv(os.getcwd() \
+                                             + '\\input_data\\' \
+                                             + 'logistics_transport_data' \
+                                             +
+                                              '\\transport_woody_hh_adv_' \
+                                             + '2040.csv',
+                                              usecols = ['feed_id',
+                                                         'sply_fips',
+                                                         'used_qnty'])
+        transport_woody_hh_adv['year'] = 2040
+        transport_woody_hh_adv['logistics_type'] = 'adv'
+        transport_woody_hh_adv['yield_type'] = 'hh'
 
-    def electricity(self, feed, logistics, yield_type):
+        ## Read in the two relevant transport tables for 2017 (only two
+        # 2017 tables are in the input data folers), add three id
+        # columns for year, logistics type and yield type, then bind
+        # together into a comprehensive transport table for use elsewhere
+        transport_herb_bc_conv = pd.read_csv(os.getcwd()\
+                                              + '\\input_data\\' \
+                                              + 'logistics_transport_data' \
+                                              + '\\transport_herb_bc_conv_' \
+                                              + '2017.csv',
+                                              usecols = ['feed_id',
+                                                         'sply_fips',
+                                                         'used_qnty'])
+        transport_herb_bc_conv['year'] = 2017
+        transport_herb_bc_conv['logistics_type'] = 'conv'
+        transport_herb_bc_conv['yield_type'] = 'bc'
+
+        transport_woody_bc_conv = pd.read_csv(os.getcwd() \
+                                             + '\\input_data\\' \
+                                             + 'logistics_transport_data' \
+                                             + '\\transport_woody_bc_conv_' \
+                                             + '2017.csv',
+                                              usecols = ['feed_id',
+                                                         'sply_fips',
+                                                         'used_qnty'])
+        transport_woody_bc_conv['year'] = 2017
+        transport_woody_bc_conv['logistics_type'] = 'conv'
+        transport_woody_bc_conv['yield_type'] = 'bc'
+
+        # bind together all the transport tables
+        # this comprehensive table is the only one to get passed around via
+        # self
+        self.transport = transport_herb_bc_adv.append(
+            transport_herb_hh_adv).append(
+            transport_woody_bc_adv).append(
+            transport_woody_hh_adv).append(
+            transport_herb_bc_conv).append(transport_woody_bc_conv)
+
+    def calc_logistics(self):
         """
+        Calculates electricity use for biomass drying and VOC emissions from
+        wood drying
+
+        Electricity:
         Tally electricity consumption from feedstock processing
         Update logistics table with these values
 
@@ -65,110 +200,46 @@ class Logistics(SaveDataHelper.SaveDataHelper):
         B = biomass production (dry short ton per yr per county)
         electricity_per_dt = electricity consumed (kWh per dry short ton?
 
-        :param yield_type: yield type for scenario
-        :param run_code: run code associated with NONROAD run
-        :param logistics: logistics type being evaluated
-        :return: True if update query is successful, False if not
-        """
-
-        # set feedstock name
-        self.kvals['feed'] = feed.lower()
-
-        # set feedstock id name for transportation table
-        self.kvals['feed_id'] = self.transport_feed_id_dict[feed]
-
-        # set transport table name
-        self.kvals['transport_table'] = self.transport_table_dict[self.feed_type_dict[feed]][yield_type][logistics] + '_%s' % (config.get('year_dict')[feed])
-
-        logger.debug('Calculating electricity consumption for {feed}'.format(feed=feed))
-
-        # set electricity consumption factor using feedstock type
-        self.kvals['electricity_per_dt'] = self.electricity_per_dt[feed][logistics]
-
-        # set logistics type
-        self.kvals['logistics'] = logistics
-
-        # set yield type
-        self.kvals['yield_type'] = yield_type
-
-        # generate string for query
-        query = """ INSERT INTO {scenario_name}.processing (fips, feed, electricity, logistics_type, yield_type)
-                    SELECT transport_data.sply_fips, '{feed}', SUM(transport_data.used_qnty * {electricity_per_dt}) AS electricity, '{logistics}', '{yield_type}'
-                    FROM {production_schema}.{transport_table} transport_data
-                    WHERE transport_data.used_qnty > 0.0 AND transport_data.feed_id = '{feed_id}'
-                    GROUP BY transport_data.sply_fips, feed_id
-                   ;""".format(**self.kvals)
-
-        # execute query
-        return self._execute_query(query)
-
-    def voc_wood_drying(self, run_code, logistics, yield_type):
-        """
+        VOC emissions:
         Compute VOC emissions from wood drying
         Only applies to forestry
 
-        Equation for computing VOC emissions from wood drying (generated by Ethan Warner):
+        Equation for computing VOC emissions from wood drying (generated
+        by Ethan Warner):
 
         VOC = (B * a) * (VOC_ef_hammermill + VOC_ef_dryer) / b
 
         VOC = VOC emissions (metric tons per year per county)
         B = biomass production (dry short ton per yr per county)
         a = constant (0.9071847 metric tons per short ton)
-        VOC_ef_h = emission factor for VOC from hammer mill (kg per dry metric ton of feedstock)
-        VOC_ef_d = emission factor for VOC from grain dryer (kg per dry metric ton of feedstock)
+        VOC_ef_h = emission factor for VOC from hammer mill (kg per dry
+        metric ton of feedstock)
+        VOC_ef_d = emission factor for VOC from grain dryer (kg per dry
+        metric ton of feedstock)
         b = constant (1000 kg per metric ton)
 
-        :param yield_type: type of yield used for scenario
-        :param run_code: run code for feedstock type
-        :param logistics: logistics type being evaluated
-        :return: True if update query is successful, False if not
+        :return: data frame with fully disaggregated electricit use and VOC
+        values, plus source category column
         """
+        transport_merged = self.transport.merge(self.electricity_per_dt,
+                                                on = ['feed_id',
+                                                      'logistics_type'],
+                                                how = 'left').merge(
+            self.voc_wood_ef, on = ['feed_id', 'logistics_type'], how = 'left')
 
-        logger.info('Computing VOC emissions from wood drying')
+        # add column containing VOC emissions from wood drying
+        # 0.9071847: metric ton per short ton
+        # 1000.0: kg per metric ton
+        transport_merged['voc'] = transport_merged['used_qnty'] * (
+            transport_merged['grain_dryer_ef'] + transport_merged[
+            'hammer_mill_ef']) * (0.9071847/1000.0)
 
-        # set feedstock id
-        feed = run_code[0:2]
+        # add column containing electricity use for biomass drying
+        transport_merged['electricity'] = transport_merged['used_qnty'] * \
+                                          transport_merged['kWh_per_dt']
 
-        # set dictionary values for string formatting
-        kvals = {'transport_table': self.transport_table_dict[self.feed_type_dict[feed]][yield_type][logistics],  # transport table
-                 'run_code': run_code,  # run code
-                 'feed': feed,  # feedstock id
-                 'feed_name': self.transport_feed_id_dict[feed],  # name of feedstock
-                 'logistics': logistics,  # logistics type
-                 'yield_type': yield_type,  # yield type
-                 'a': 0.9071847,  # metric ton per short ton
-                 'b': 1000.0,  # kg per metric ton
-                 'VOC_ef_h': self.voc_wood_ef[logistics]['hammer_mill'],  # VOC emission factor for hammer mill
-                 'VOC_ef_d': self.voc_wood_ef[logistics]['grain_dryer'],  # VOC emission factor for grain dryer
-                 'scenario_name': self.kvals['scenario_name'],
-                 'production_schema': self.kvals['production_schema'],
-                 'year': config.get('year_dict')['all_crops']
-                 }
+        # add column defining source category and logistics type
+        transport_merged['source_category'] = 'Pre-processing, ' \
+                                              + transport_merged.logistics_type
 
-        # generate string for query and append to queries
-        query = """UPDATE {scenario_name}.processing process
-                LEFT JOIN (SELECT sply_fips, SUM(used_qnty) AS used_qnty, feed_id
-                           FROM {production_schema}.{transport_table}_{year}
-                           GROUP BY sply_fips, feed_id) transport_data
-                ON process.fips = transport_data.sply_fips
-                SET voc_wood = transport_data.used_qnty * {a} * ({VOC_ef_h} + {VOC_ef_d}) / {b}
-                WHERE   logistics_type = '{logistics}'
-                        AND yield_type = '{yield_type}'
-                        AND feed = '{feed}'
-                        AND feed_id = '{feed_name}';""".format(**kvals)
-
-        return self._execute_query(query)
-
-    def calc_logistics(self, logistics_list):
-        # Execute wood drying and electricity functions for all feedstocks in feedstock list
-        logger.info('Evaluating logistics')
-
-        for feed in self.feedstock_list:
-            if self.transport_feed_id_dict[feed] != 'None':
-                for logistics_type in logistics_list:
-                    # compute electricity
-                    self.electricity(feed, logistics_type, self.yield_type)
-
-                    if feed.startswith('F'):
-                        self.voc_wood_drying(feed, logistics_type, self.yield_type)
-
+        return transport_merged
