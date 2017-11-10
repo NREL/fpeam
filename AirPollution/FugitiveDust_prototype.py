f5ab729caed509f899235aa1d5b711963301d67a
rjhanes <rebecca.hanes@nrel.gov>
2017-11-10 14:12:16 -0700

    moves calc function inside class, adjusts variable names, adds return to calc function

--- AirPollution/FugitiveDust_prototype.py
+++ AirPollution/FugitiveDust_prototype.py
@@ -77,7 +77,7 @@ class FugitiveDust:
         # read in complete budget data set
         # eventually this df will need budget data for all crops of interest
         # @todo replace with default or user-defined data pulled from database
-        budg_dat = pd.read_csv(os.getcwd() +
+        self.budg_dat = pd.read_csv(os.getcwd() +
                                '\\input_data\\equip_budgets\\' +
                                'bdgtconv_20151215.csv')
 
@@ -86,111 +86,123 @@ class FugitiveDust:
         # ("R") operation.
         # this will be used in allocating fugdust emissions between crops and
         # residues for sorghum (stubble), corn (stover) and wheat (straw)
-        budg_dat['croptype'] = budg_dat.opertype.str[3]
+        self.budg_dat['croptype'] = self.budg_dat.opertype.str[3]
 
         # use the croptype column to rename budget entries that correspond
         # to corn stover, sorghum stubble and wheat straw
-        budg_dat.loc[(budg_dat['croptype'] == 'R') &
-                     (budg_dat['crop'] == 'Corn'), 'crop'] = 'Corn stover'
+        self.budg_dat.loc[(self.budg_dat['croptype'] == 'R') &
+                     (self.budg_dat['crop'] == 'Corn'), 'crop'] = 'Corn stover'
 
-        budg_dat.loc[(budg_dat['croptype'] == 'R') &
-                     (budg_dat['crop'] == 'Sorghum'), 'crop'] = 'Sorghum ' \
-                                                                'stubble'
+        self.budg_dat.loc[(self.budg_dat['croptype'] == 'R') &
+                     (self.budg_dat['crop'] == 'Sorghum'), 'crop'] = \
+            'Sorghum stubble'
 
-        budg_dat.loc[(budg_dat['croptype'] == 'R') &
-                     (budg_dat['crop'] == 'Wheat'), 'crop'] = 'Wheat straw'
+        self.budg_dat.loc[(self.budg_dat['croptype'] == 'R') &
+                     (self.budg_dat['crop'] == 'Wheat'), 'crop'] = \
+            'Wheat straw'
 
-        self.budg_dat = budg_dat
+        # calculate horsepower-hours for each opertype
+        # fillna replaces all missing values with zeros
+        # otherwise the entire calculation is filled with NaNs
+        self.budg_dat['hp_hrs'] = self.budg_dat.powr01_hpac.fillna(0) * \
+                                  self.budg_dat.time01_hrac.fillna(0) + \
+                                  self.budg_dat.powr02_hpac.fillna(0) * \
+                                  self.budg_dat.time02_hrac.fillna(0) + \
+                                  self.budg_dat.powr03_hpac.fillna(0) * \
+                                  self.budg_dat.time03_hrac.fillna(0)
 
-def set_fugdust_emissions(self):
+        # this sums the hp_hrs values within each group defined by the
+        # budget id, then calculates the PM10 allocation factor by dividing
+        # each individual hp_hrs value by the budget id group total.
+        # tested calculation using BA.CT.01, math checked out
+        self.budg_dat['hp_hrs_alloc'] = \
+            self.budg_dat.groupby('bdgt_id').hp_hrs.apply(lambda x:
+                                                          x / float(x.sum()))
 
-    # calculate horsepower-hours for each opertype
-    # fillna replaces all missing values with zeros
-    # otherwise the entire calculation is filled with NaNs
-    self.budg_dat['hp_hrs'] = self.budg_dat.powr01_hpac.fillna(0) * \
-                              self.budg_dat.time01_hrac.fillna(0) + \
-                              self.budg_dat.powr02_hpac.fillna(0) * \
-                              self.budg_dat.time02_hrac.fillna(0) + \
-                              self.budg_dat.powr03_hpac.fillna(0) * \
-                              self.budg_dat.time03_hrac.fillna(0)
+        # merge the emission factors from fugdust_ef into budg_dat
+        # cheat a bit by dividing the budget into one budget for harvest
+        # activities and another for non-harvest activities
+        budg_dat_harv = self.budg_dat.loc[self.budg_dat.opertype.isin(('HRVR',
+                                                                       'HRVC'))]
+        budg_dat_nonharv = self.budg_dat.loc[
+            self.budg_dat.opertype.isin(('ESTC',
+                                         'MNTR',
+                                         'MNTC'))]
 
-    # this sums the hp_hrs values within each group defined by the budget id,
-    # then calculates the PM10 allocation factor by dividing each individual
-    # hp_hrs value by the budget id group total.
-    # tested calculation using BA.CT.01, math checked out
-    self.budg_dat['hp_hrs_alloc'] = \
-        self.budg_dat.groupby('bdgt_id').hp_hrs.apply(lambda x: x /
-                                                                float(x.sum()))
+        # similarly divide the fugdust_ef into harvest and non-harvest data
+        #  frames
+        fugdust_ef_harv = self.fugdust_ef.loc[
+            self.fugdust_ef.source_category ==
+            'Harvest']
+        fugdust_ef_nonharv = self.fugdust_ef.loc[
+            self.fugdust_ef.source_category ==
+            'Non-Harvest']
 
-    # merge the emission factors from fugdust_ef into budg_dat
-    # cheat a bit by dividing the budget into one budget for harvest activities
-    # and another for non-harvest activities
-    budg_dat_harv = self.budg_dat.loc[self.budg_dat.opertype.isin(('HRVR',
-                                                                   'HRVC'))]
-    budg_dat_nonharv = self.budg_dat.loc[self.budg_dat.opertype.isin(('ESTC',
-                                                                      'MNTR',
-                                                                      'MNTC'))]
+        # merge by matching crop, till and budget year - ignore polyfrr
+        budg_dat_harv = budg_dat_harv.merge(fugdust_ef_harv,
+                                            on=['crop', 'till',
+                                                'bdgtyr'],
+                                            how='outer')
 
-    # similarly divide the fugdust_ef into harvest and non-harvest data frames
-    fugdust_ef_harv = self.fugdust_ef.loc[self.fugdust_ef.source_category ==
-                                          'Harvest']
-    fugdust_ef_nonharv = self.fugdust_ef.loc[self.fugdust_ef.source_category ==
-                                             'Non-Harvest']
+        # calculate pm10 emissions (lb per acre) from each opertype
+        # these values do NOT include pm10 from on-farm travel, which is
+        # calculated for each state after the production data is merged in
+        budg_dat_harv['pm10_lbac_alloc'] = budg_dat_harv.pm10_ef_lbac * \
+                                           budg_dat_harv.hp_hrs_alloc
 
-    # merge by matching crop, till and budget year - ignore polyfrr
-    budg_dat_harv = budg_dat_harv.merge(fugdust_ef_harv, on = ['crop', 'till',
-                                                               'bdgtyr'],
-                                        how = 'outer')
+        # merge non-harvest dfs by matching crop, till and budget year
+        budg_dat_nonharv = budg_dat_nonharv.merge(fugdust_ef_nonharv,
+                                                  on=['crop',
+                                                      'till',
+                                                      'bdgtyr'],
+                                                  how='outer')
 
-    # calculate pm10 emissions (lb per acre) from each opertype
-    # these values do NOT include pm10 from on-farm travel, which is calculated
-    # for each state after the production data is merged in
-    budg_dat_harv['pm10_lbac_alloc'] = budg_dat_harv.pm10_ef_lbac * \
-                             budg_dat_harv.hp_hrs_alloc
+        # calculate pm10 emissions (lb per acre) from each opertype
+        # these values do NOT include pm10 from on-farm travel, which is
+        # calculated for each state after the production data is merged in
+        budg_dat_nonharv['pm10_lbac_alloc'] = budg_dat_nonharv.pm10_ef_lbac * \
+                                              budg_dat_nonharv.hp_hrs_alloc
 
-    # merge non-harvest dfs by matching crop, till and budget year
-    budg_dat_nonharv = budg_dat_nonharv.merge(fugdust_ef_nonharv,
-                                              on = ['crop',
-                                                    'till',
-                                                    'bdgtyr'],
-                                              how = 'outer')
+        # stitch together the harvest and non-harvest expanded budgets with
+        #  pm10
+        self.budg_dat_fugdust = budg_dat_harv.append(budg_dat_nonharv)
 
-    # calculate pm10 emissions (lb per acre) from each opertype
-    # these values do NOT include pm10 from on-farm travel, which is calculated
-    # for each state after the production data is merged in
-    budg_dat_nonharv['pm10_lbac_alloc'] = budg_dat_nonharv.pm10_ef_lbac * \
-                               budg_dat_nonharv.hp_hrs_alloc
+        # calculate PM2.5 emissions (lb per acre) as 20% by mass of PM10
+        # emissions
+        self.budg_dat_fugdust['pm25_lbac_alloc'] = \
+            0.20 * self.budg_dat_fugdust.pm10_lbac_alloc
 
-    # stitch together the harvest and non-harvest expanded budgets with pm10
-    self.budg_dat_fugdust = budg_dat_harv.append(budg_dat_nonharv)
+    def set_fugdust_emissions(self):
+        """
 
-    # calculate PM2.5 emissions (lb per acre) as 20% by mass of PM10 emissions
-    self.budg_dat_fugdust['pm25_lbac_alloc'] = \
-        0.20 * self.budg_dat_fugdust.pm10_lbac_alloc
 
-    # merge together the production data with state FIPS, the budget data with
-    # harvest and non-harvest PM10 and PM2.5 emissions (pounds per acre) and
-    # the silt table with the silt information necessary to calculate PM10 and
-    # PM2.5 from on-farm travel
-    self.output_df = self.prod_dat.merge(self.budg_dat_fugdust,
-                                   on = ['crop', 'till', 'polyfrr'],
-                                   how = 'outer').merge(self.silt_table,
-                                                        on = 'st_fips',
-                                                        how = 'outer')
+        :return: output_df, containing PM10 and PM25 emissions from harvest
+        and non-harvest activities allocated by opertype
+        """
+    
+        # merge together the production data with state FIPS, the budget
+        # data  with harvest and non-harvest PM10 and PM2.5 emissions
+        # (pounds per acre)
+        output_df = self.prod_dat.merge(self.budg_dat_fugdust,
+                                       on = ['crop', 'till', 'polyfrr'],
+                                       how = 'outer').merge(self.silt_table,
+                                                            on = 'st_fips',
+                                                            how = 'outer')
+        
+        # calculate pm10 by multiplying the PM10 emissions factors (which
+        # include emissions from harvest, non-harvest and lime application
+        # activities) by harvested acres and adding PM10 from on-farm travel
+        #  (also allocated between opertypes by horsepower-hours),
+        # then converting the sum to metric tons
+        output_df['pm10'] = (output_df.harv * output_df.pm10_lbac_alloc) * \
+                                 self.convert_lb_to_mt
+    
+        # calculate PM2.5 by multiplying the PM2.5 emissions (including
+        # emissions from harvest, non-harvest and lime application
+        # activities) by harvested acres and adding the PM2.5 from on-farm
+        # travel (also allocated between opertypes by horsepower-hours),
+        # then converting the sum to metric tons
+        output_df['pm25'] = (output_df.harv * output_df.pm25_lbac_alloc) * \
+                                 self.convert_lb_to_mt
 
-    # calculate pm10 by multiplying the PM10 emissions factors (which include
-    # emissions from harvest, non-harvest and lime application activities) by
-    # harvested acres and adding PM10 from on-farm travel (also allocated
-    # between opertypes by horsepower-hours), then converting the sum to
-    # metric tons
-    self.output_df['pm10'] = (self.output_df.harv *
-                              self.output_df.pm10_lbac_alloc) * \
-                             self.convert_lb_to_mt
-
-    # calculate PM2.5 by multiplying the PM2.5 emissions (including emissions
-    # from harvest, non-harvest and lime application activities) by harvested
-    # acres and adding the PM2.5 from on-farm travel (also allocated between
-    # opertypes by horsepower-hours), then converting the sum to metric tons
-    self.output_df['pm25'] = (self.output_df.harv *
-                              self.output_df.pm25_lbac_alloc) * \
-                             self.convert_lb_to_mt
+        return output_df
