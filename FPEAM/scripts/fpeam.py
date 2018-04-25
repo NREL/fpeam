de9efa195099b0689021681d01706dffd2a2abc4
Dylan Hettinger <dylan.hetting@nrel.gov>
2018-04-25 17:48:59 -0600

    add option groups and configs

--- FPEAM/scripts/fpeam.py
+++ FPEAM/scripts/fpeam.py
@@ -7,27 +7,41 @@ from FPEAM import (Data, IO, FPEAM)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Execute FPEAM model against a given equipement'
+        description='Execute FPEAM model against a given equipment'
                     ' budget and production'
-                    ' dataset and configuration file')
+                    ' dataset and configuration files')
 
     parser.add_argument('budget', type=str, help='equipment budget')
     parser.add_argument('production', type=str, help='production data')
     parser.add_argument('run_config', type=str, help='execution configuration')
 
-    parser.add_argument('--fpeam_config', type=str,
-                        default='../configs/fpeam.ini',
-                        help='FPEAM model configuration options')
-    parser.add_argument('--moves_config', type=str,
-                        default='../configs/moves.ini',
-                        help='MOVES model configuration options')
-    parser.add_argument('--nonroad_config', type=str,
-                        default='../configs/nonroad.ini',
-                        help='NONROAD configuration options')
-    parser.add_argument('--chemical_config', type=str,
-                        help='chemical module configuration options')
-    parser.add_argument('--fugitivedust_config', type=str,
-                        help='fugitive dust module configuration options')
+    data_group = parser.add_argument_group('data files')
+    data_group.add_argument('--emission_factors', type=str,
+                            default='../data/emission_factors.csv',
+                            help='emission factor values')
+    data_group.add_argument('--fertilizer_distribution', type=str,
+                            default='../data/fertilizer_distribution.csv',
+                            help='fertilzer distribution percentage values')
+    data_group.add_argument('--fugitive_dust', type=str,
+                            default='../data/fugitive_dust_emission_factors.csv',
+                            help='fugitive dust emission factors values')
+
+    config_group = parser.add_argument_group('configuration files')
+    config_group.add_argument('--fpeam_config', type=str,
+                              default='../configs/fpeam.ini',
+                              help='FPEAM model configuration options')
+    config_group.add_argument('--moves_config', type=str,
+                              default='../configs/moves.ini',
+                              help='MOVES model configuration options')
+    config_group.add_argument('--nonroad_config', type=str,
+                              default='../configs/nonroad.ini',
+                              help='NONROAD configuration options')
+    config_group.add_argument('--chemical_config', type=str,
+                              default='../configs/chemical.ini',
+                              help='chemical module configuration options')
+    config_group.add_argument('--fugitivedust_config', type=str,
+                              default='../configs/fugitivedust.ini',
+                              help='fugitive dust module configuration options')
 
     args = parser.parse_args()
 
