a413c6b9d089ab3936203dc782bde9d4608f35e5
rjhanes <rebecca.hanes@nrel.gov>
2018-07-23 14:41:24 -0600

    removes OnFarm and OnRoad classes, updates docstring

--- FPEAM/Modules/FugitiveDust.py
+++ FPEAM/Modules/FugitiveDust.py
@@ -6,7 +6,7 @@ LOGGER = utils.logger(name=__name__)
 
 
 class FugitiveDust(Module):
-    """Base class to manage execution of Transporation emissions"""
+    """Base class to manage execution of on-farm fugitive dust calculations"""
 
     def __init__(self, config, **kvals):
 
@@ -18,52 +18,6 @@ class FugitiveDust(Module):
         return
 
     def _post_process(self):
-        return
-
-    # def __enter__(self):
-    #     return self
-    #
-    # def __exit__(self, exc_type, exc_val, exc_tb):
-    #     # process exceptions
-    #     if exc_type is not None:
-    #         LOGGER.exception('%s\n%s\n%s' % (exc_type, exc_val, exc_tb))
-    #         return False
-    #     else:
-    #         return self
-
-
-class OnFarmFugitiveDust(FugitiveDust):
-    """Class to manage execution of on-farm fugitive dust emissions"""
-
-    def __init__(self, config):
-        # init parent
-        super(FugitiveDust, self).__init__(config=config)
-
-    def proceess(self):
-        # calculate emissions
-        return
-
-    # def __enter__(self):
-    #     return self
-    #
-    # def __exit__(self, exc_type, exc_val, exc_tb):
-    #     # process exceptions
-    #     if exc_type is not None:
-    #         LOGGER.exception('%s\n%s\n%s' % (exc_type, exc_val, exc_tb))
-    #         return False
-    #     else:
-    #         return self
-
-
-class OnRoadFugitiveDust(FugitiveDust):
-    """Class to manage execution of on-road fugitive dust emissions"""
-
-    def __init__(self, config):
-        # init parent
-        super(FugitiveDust, self).__init__(config=config)
-
-    def process(self):
-        # calculate
         return
 
     # def __enter__(self):
