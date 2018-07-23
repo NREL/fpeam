e02f7c2922665fecdb503a2663e41f95fd6afbb8
rjhanes <rebecca.hanes@nrel.gov>
2018-07-23 14:50:10 -0600

    adds get_fugitivedust method

--- FPEAM/Modules/FugitiveDust.py
+++ FPEAM/Modules/FugitiveDust.py
@@ -8,25 +8,29 @@ LOGGER = utils.logger(name=__name__)
 class FugitiveDust(Module):
     """Base class to manage execution of on-farm fugitive dust calculations"""
 
-    def __init__(self, config, **kvals):
+    def __init__(self, config, production,
+                 fugitive_dust_emission_factors,
+                 **kvals):
+        """
+        :param config [ConfigObj] configuration options
+        :param production: [DataFrame] production values
+        :param fugitive_dust_emission_factors: [DataFrame] fugitive dust
+        generation per acre
+        """
 
         # init parent
-        super(FugitiveDust, self).__init__(config=config)
+        super(FugitiveDust, self).__init__(config = config)
 
-    def _pre_process(self):
-        # merges budget and product data
-        return
+        # init properties
+        self.production = production
+        self.fugitivedust = fugitive_dust_emission_factors
 
-    def _post_process(self):
-        return
+    def get_fugitivedust(self):
+        """
+        Calculate all total PM10 and PM2.5 emissions from fugitive dust by
+        feedstock, region and tillage type.
 
-    # def __enter__(self):
-    #     return self
-    #
-    # def __exit__(self, exc_type, exc_val, exc_tb):
-    #     # process exceptions
-    #     if exc_type is not None:
-    #         LOGGER.exception('%s\n%s\n%s' % (exc_type, exc_val, exc_tb))
-    #         return False
-    #     else:
-    #         return self
+        :return: _df: DataFrame containing PM10 and PM2.5 amounts
+        """
+
+        _idx = ['feedstock', 'tillage_type']
