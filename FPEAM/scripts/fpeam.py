7a6456232b74e56573619aa5e8d4f8e26b94cae2
Dylan Hettinger <dylan.hetting@nrel.gov>
2018-08-27 14:15:35 -0600

    cleanup

--- FPEAM/scripts/fpeam.py
+++ FPEAM/scripts/fpeam.py
@@ -4,7 +4,7 @@ import argparse
 import logging
 import os
 
-from FPEAM import (Data, IO, FPEAM, utils)
+from FPEAM import (IO, FPEAM, utils)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
