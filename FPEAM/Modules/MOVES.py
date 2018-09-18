1d4d2bfed0065c9109f08ab1f26da82a00255495
rjhanes <rebecca.hanes@nrel.gov>
2018-09-18 14:48:53 -0600

    corrects state column creation to avoid a slice error from pandas

--- FPEAM/Modules/MOVES.py
+++ FPEAM/Modules/MOVES.py
@@ -1128,8 +1128,11 @@ class MOVES(Module):
 
         # add state column to both tables by pulling out first two digits of
         #  MOVESScenarioID
-        _rateperdistance['state'] = _rateperdistance.fips.str.slice(stop=2)
-        _ratepervehicle['state'] = _ratepervehicle.fips.str.slice(stop=2)
+        # do this in two steps to avoid a warning from pandas
+        _rateperdistance_state = _rateperdistance['fips'].str[:2]
+        _rateperdistance['state'] = _rateperdistance_state
+        _ratepervehicle_state = _ratepervehicle['fips'].str[:2]
+        _ratepervehicle['state'] = _ratepervehicle_state
 
         # create the average speed table that will be used in calculating
         # the average emissions rate per distance
