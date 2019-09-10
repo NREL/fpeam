6ad0166c565fb1995e59fa23d694a98c67be316c
rjhanes <rebecca.hanes@nrel.gov>
2019-09-10 09:01:22 -0600

    creates Booleans to write to moves.ini, closes #152 pending full test

--- src/FPEAM/gui/MovesConfig.py
+++ src/FPEAM/gui/MovesConfig.py
@@ -6,8 +6,11 @@ def movesConfigCreation(tmpFolder, attributeValueObj):
 
     ### MOVES execution options
 
-    ## Aggregation Level
-    #aggregation_level = '{aggregation_level}'
+    ## use a single representative county for all counties in each state
+    #moves_by_state = '{aggregation_level_state}'
+    
+    ## use a single representative county for each crop for all counties in each state
+    #moves_by_state_and_feedstock = '{aggregation_level_state_feedstock}'
 
     ## use existing results in MOVES output database or run MOVES for all counties
     #use_cached_results = {use_cached_results}
@@ -63,7 +66,20 @@ def movesConfigCreation(tmpFolder, attributeValueObj):
     #ending_hour = {ending_hour}
     #day_type = {day_type} """
 
-    my_ini_config = ini_template_string.format(aggregation_level=attributeValueObj.aggegationLevel,
+    # reformat the aggregation level input into two Booleans
+    if attributeValueObj.aggegationLevel == 'By State':
+        _aggregation_level_state = True
+        _aggregation_level_state_feedstock = False
+    elif attributeValueObj.aggegationLevel == 'By State-Feedstock':
+        _aggregation_level_state = False
+        _aggregation_level_state_feedstock = True
+    else:
+        _aggregation_level_state = False
+        _aggregation_level_state_feedstock = False
+
+
+    my_ini_config = ini_template_string.format(aggregation_level_state=_aggregation_level_state,
+                                               aggregation_level_state_feedstock=_aggregation_level_state_feedstock,
                                                use_cached_results=attributeValueObj.cachedResults,
                                                feedstock_measure_type=attributeValueObj.feedstockMeasureTypeMoves,
                                                VMT_per_truck=attributeValueObj.vMTPerTruck,
