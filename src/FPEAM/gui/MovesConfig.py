fb3f22ec2e9145835547d731ce1df2286865ebdf
rjhanes <rebecca.hanes@nrel.gov>
2019-09-16 10:55:53 -0600

    adds inputs and changes variable names to match moves.spec #160

--- src/FPEAM/gui/MovesConfig.py
+++ src/FPEAM/gui/MovesConfig.py
@@ -2,71 +2,105 @@ import os, tempfile
 
 #Create MOVES config file
 def movesConfigCreation(tmpFolder, attributeValueObj):
-    ini_template_string = """[moves]
 
+    ini_template_string = """[moves]
+    # run identifier
+    scenario_name = '{scenario_name}'
+    
+    
     ### MOVES execution options
 
     ## use a single representative county for all counties in each state
-    #moves_by_state = {aggregation_level_state}
+    moves_by_state = {aggregation_level_state}
     
     ## use a single representative county for each crop for all counties in each state
-    #moves_by_state_and_feedstock = {aggregation_level_state_feedstock}
+    moves_by_state_and_feedstock = {aggregation_level_state_feedstock}
 
     ## use existing results in MOVES output database or run MOVES for all counties
-    #use_cached_results = {use_cached_results}
+    use_cached_results = {use_cached_results}
 
     ## production table identifier (feedstock_measure in production data)
-    #feedstock_measure_type = '{feedstock_measure_type}'
+    feedstock_measure_type = '{feedstock_measure_type}'
 
-    ## Number of trucks used
-    #No_of_trucks_used = {No_of_trucks_used}
+    ## population of combination short-haul trucks per trip
+    pop_short_haul = {no_of_trucks_used}
+    
+    ## vehicle category: combination trucks
+    hpmsv_type_id = 60
+    
+    ## specific vehicle type: short-haul combination truck
+    source_type_id = 61
 
     ## start year (equipment year #1)
-    #year = {year}
-
-    #moves_path = '{moves_path}'
-    #moves_datafiles_path = '{moves_datafiles_path}'
+    year = {year}
     
     ### MOVES database connection options
-    #moves_db_host = '{moves_db_host}'
-    #moves_db_user = '{moves_db_user}'
-    #moves_db_pass = '{moves_db_pass}'
-    #moves_database = '{moves_database}'
-    #moves_output_db = '{moves_output_db}'
-
-
+    moves_db_host = '{moves_db_host}'
+    moves_db_user = '{moves_db_user}'
+    moves_db_pass = '{moves_db_pass}'
+    moves_database = '{moves_database}'
+    moves_output_db = '{moves_output_db}'
+    
+    
+    ### MOVES application options
+    
+    ## the moves version used only for human reference; it's ignored by MOVES
+    moves_version = 'MOVES'
+    
+    ## this directory contains all input files created for MOVES runs
+    moves_datafiles_path = '{moves_datafiles_path}'
+    
+    ## use this path to specify which version of MOVES should be run
+    moves_path = '{moves_path}'
+    
+    ### MySQL options
+    mysql_binary = ""
+    mysqldump_binary = ""
+    
+    
     ### input files
 
     ## fuel fraction by engine type
-    #avft = '{avft}'
+    avft = '{avft}'
 
     ## production region to MOVES FIPS mapping
-    #region_fips_map = '{region_fips_map}'
-
-
+    region_fips_map = '{region_fips_map}'
+    
+    
     ### MOVES input options
 
     ## fraction of VMT by road type (must sum to 1)
-    #[vmt_fraction]
-    #rural_restricted = {rural_restricted}
-    #rural_unrestricted = {rural_unrestricted}
-    #urban_restricted = {urban_restricted}
-    #urban_unrestricted = {urban_unrestricted}
+    [vmt_fraction]
+    rural_restricted = {rural_restricted}
+    rural_unrestricted = {rural_unrestricted}
+    urban_restricted = {urban_restricted}
+    urban_unrestricted = {urban_unrestricted}
 
     ## timespan(s)
-    #[moves_timespan]
-    #month = {month}
-    #date = {date}
-    #beginning_hour = {beginning_hour}
-    #ending_hour = {ending_hour}
-    #day_type = {day_type} """
+    [moves_timespan]
+    month = {month}
+    day = {date}
+    beginning_hour = {beginning_hour}
+    ending_hour = {ending_hour}
+    
+    # MOVES pollutant dictionary (pollutant name to pollutant ID)
+    [pollutant_dict]
+    NH3 = 30
+    CO = 2
+    NOX = 3
+    PM10 = 100
+    PM25 = 110
+    SO2 = 31
+    VOC = 87
+    """
 
 
-    my_ini_config = ini_template_string.format(aggregation_level_state=attributeValueObj.aggregation_level_state,
+    my_ini_config = ini_template_string.format(scenario_name=attributeValueObj.scenarioName,
+                                               aggregation_level_state=attributeValueObj.aggregation_level_state,
                                                aggregation_level_state_feedstock=attributeValueObj.aggregation_level_state_feedstock,
                                                use_cached_results=attributeValueObj.cachedResults,
                                                feedstock_measure_type=attributeValueObj.feedstockMeasureTypeMoves,
-                                               No_of_trucks_used=attributeValueObj.noOfTrucksUsed,
+                                               no_of_trucks_used=attributeValueObj.noOfTrucksUsed,
                                                year=attributeValueObj.yearMoves,
                                                moves_path=attributeValueObj.movesPath,
                                                moves_datafiles_path=attributeValueObj.movesDatafilesPath,
